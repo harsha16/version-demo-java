61aa6f4da7da75fe0d12355892f91916017e8006
harshagen <harsha@genesiis.com>
2020-01-28 14:43:15 +0530

    first commit

---
+++ src/test/java/com/example/versiondemojava/VersionDemoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.versiondemojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersionDemoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
